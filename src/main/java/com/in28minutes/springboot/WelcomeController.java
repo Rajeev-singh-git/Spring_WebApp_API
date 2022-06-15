25077c02424d1b27fb3485ff98f1c3652ffcb468
Rajeev Singh <rajeevsinghup30@gmail.com>
2022-06-15 09:09:15 +0530

    Step 7 : Rest service with @GetMapping and @PathVariable

--- src/main/java/com/in28minutes/springboot/WelcomeController.java
+++ src/main/java/com/in28minutes/springboot/WelcomeController.java
@@ -1,5 +1,6 @@
 package com.in28minutes.springboot;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +10,7 @@ import com.in28minutes.service.WelcomeService;
 @RestController //Creates Rest controller
 public class WelcomeController {
 	
-	@Autowired //It inject bean created by Spring. Line no=21 (Creates an instance of class and Autowire it wherever needed.
+	@Autowired //It inject bean created by Spring.
 	private WelcomeService service; //	private WelcomeService service = new  WelcomeService;
 	
 	@RequestMapping("/welcome") //maps the URL = http://localhost:8080/welcome
---
+++ src/main/java/com/in28minutes/springboot/controller/SurveyController.java
@@ -0,0 +1,38 @@
+package com.in28minutes.springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.in28minutes.springboot.service.SurveyService;
+import com.in28minutes.springboot.model.Question;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RestController
+public class SurveyController {
+	
+	@Autowired
+	private SurveyService surveyService;
+	
+	@GetMapping("/surveys/{surveyId}/questions")
+	public List<Question> retrieveQuestionsForSurvey(@PathVariable String surveyId){
+		return surveyService.retrieveQuestions(surveyId);
+		
+	}
+	
+	
+
+}
+
+/* 
+ URL = /surveys/Survey1/questions/
+ SurveyService - Autowire
+ PathVariable  -  surveyId
+ Path variable is used when we get variable as input
+ 
+ */
+
+
