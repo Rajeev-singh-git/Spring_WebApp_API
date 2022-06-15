057945a55d60a94d39c18ed1cde692e021b1e73b
Rajeev Singh <rajeevsinghup30@gmail.com>
2022-06-16 00:02:26 +0530

    Step 15.1 : Receicve welcome message from application.properties           Receicve welcome message from program arguments.

--- src/main/java/com/in28minutes/service/WelcomeService.java
+++ src/main/java/com/in28minutes/service/WelcomeService.java
@@ -1,11 +1,16 @@
 package com.in28minutes.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component //creates instance of the class (beans)
 public	class WelcomeService{
+	
+	@Value("${welcome.message}") //Autowires property value 
+	private String welcomeMessage;
+	
 		public String retrieveWelcomeMessage() {
 			// Business logic, complex methods are written in service
-			return "Good Morning! Have a wonderful day";
+			return welcomeMessage;
 		}
 }
