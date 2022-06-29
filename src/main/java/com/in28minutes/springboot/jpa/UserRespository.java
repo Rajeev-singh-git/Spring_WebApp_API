f826abed75f825954d49f74f9109f76ab29a9263
Rajeev Singh <rajeevsinghup30@gmail.com>
2022-06-29 23:44:48 +0530

    Step 24: Writing Unit Tests with Spring Boot and Mockito Part 2

--- src/main/java/com/in28minutes/springboot/jpa/UserRespository.java
+++ src/main/java/com/in28minutes/springboot/jpa/UserRespository.java
@@ -1,9 +1,11 @@
 package com.in28minutes.springboot.jpa;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface UserRespository extends CrudRepository <User, Long>{
 
 	List<User> findByRole(String role);
