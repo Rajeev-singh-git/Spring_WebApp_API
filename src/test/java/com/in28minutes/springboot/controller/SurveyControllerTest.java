bd37c05551c8636139abc91141127e19aa510861
Rajeev Singh <rajeevsinghup30@gmail.com>
2022-07-03 10:30:42 +0530

    Step 27: Configure Spring Security user roles - Part 2

--- src/test/java/com/in28minutes/springboot/controller/SurveyControllerTest.java
+++ src/test/java/com/in28minutes/springboot/controller/SurveyControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -26,6 +27,7 @@ import com.in28minutes.springboot.service.SurveyService;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(value = SurveyController.class)
+@WithMockUser
 public class SurveyControllerTest {
 
 	@Autowired
